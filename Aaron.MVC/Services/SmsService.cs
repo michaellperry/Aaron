ce2a3ecfb43183573bcbbacf8942d79e7b4d5287
Michael L Perry <michael@qedcode.com>
2013-06-16 21:03:49 -0500

    Don't call Twilio with default settings.

--- Aaron.MVC/Services/SmsService.cs
+++ Aaron.MVC/Services/SmsService.cs
@@ -12,12 +12,15 @@ namespace Aaron.MVC.Services
             if (phone.Length > 10)
                 phone = "+" + phone;
 
-            string TwilioPhone = WebConfigurationManager.AppSettings["TwilioPhone"];
-            string AccountSid = WebConfigurationManager.AppSettings["TwilioAccountSid"];
-            string AuthToken = WebConfigurationManager.AppSettings["TwilioAuthToken"];
+            string twilioPhone = WebConfigurationManager.AppSettings["TwilioPhone"];
+            string accountSid = WebConfigurationManager.AppSettings["TwilioAccountSid"];
+            string authToken = WebConfigurationManager.AppSettings["TwilioAuthToken"];
 
-            var twilio = new TwilioRestClient(AccountSid, AuthToken);
-            twilio.SendSmsMessage(TwilioPhone, phone, body, MessageSent);
+            if (authToken == "secret")
+                return;
+
+            var twilio = new TwilioRestClient(accountSid, authToken);
+            twilio.SendSmsMessage(twilioPhone, phone, body, MessageSent);
         }
 
         private static void MessageSent(SMSMessage e)
